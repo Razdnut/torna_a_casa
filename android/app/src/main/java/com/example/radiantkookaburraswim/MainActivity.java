8b54f6f98cf760102d910717bcb5abc0b0c889cf
[dyad] <andor@venclous.org>
2025-08-05 17:03:26 +0200

    [dyad] add Capacitor for mobile app support

---
+++ android/app/src/main/java/com/example/radiantkookaburraswim/MainActivity.java
@@ -0,0 +1,5 @@
+package com.example.radiantkookaburraswim;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.radiantkookaburraswim',
+  appName: 'radiant-kookaburra-swim',
+  webDir: 'dist'
+};
+
+export default config;
